c8c6e59003bb989f07867a27e558f5da9d909200
praisesun <blackstar934589@gmail.com>
2024-03-12 14:55:44 +0200

    додати "увійти"

--- app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ipz_cw_2.ui.theme.IPZ_CW_2_Diak_VladyslavTheme
@@ -30,4 +35,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SignIn(modifier: Modifier = Modifier) {}
+fun SignIn(modifier: Modifier = Modifier) {
+    Column {
+        TextField(value = "", onValueChange = {}, placeholder = { Text("Введіть пошту") })
+        TextField(value = "", onValueChange = {}, placeholder = { Text("Введіть пароль") })
+        Button(onClick = {}) { Text(text = "Увійти") }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignInPreview() {
+    IPZ_CW_2_Diak_VladyslavTheme {
+        SignIn()
+    }
+}
